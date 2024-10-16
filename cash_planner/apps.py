f68db60808465ca8b63c646bb865e20a6ac93e81
1F10220295-Kota <107393944+1F10220295-Kota@users.noreply.github.com>
2024-10-16 18:33:30 +0900

    Delete

--- cash_planner/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CashPlannerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cash_planner'
-
-    def ready(self):
-        import cash_planner.signals
--- cash_planner/static/js/main.js
+++
@@ -1,8 +0,0 @@
-function openPopup() {
-    const width = 600;
-    const height = 400;
-    const left = (window.screen.width / 2) - (width / 2);
-    const top = (window.screen.height / 2) - (height / 2);
-    window.open("{% url 'add_expense' %}", 'ポップアップ', 
-                `width=${width},height=${height},top=${top},left=${left}`);
-}
