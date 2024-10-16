cdbd27cdbe95068b2bc3d3384634eba8bb341e52
s1f10220245 <107395647+s1f10220245@users.noreply.github.com>
2024-10-16 18:57:30 +0900

    fix config

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from cash_planner import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cash_planner.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),  # Django の認証ビューを使用
+    # path('accounts/', include('django.contrib.auth.urls')),  # Django の認証ビューを使用
 ]
